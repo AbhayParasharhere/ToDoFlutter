7c9947ab657cf51c258f39f23554def61c1c14cb
AbhayParasharhere <83302622+AbhayParasharhere@users.noreply.github.com>
2024-05-14 13:30:08 -0700

    Test grid view builder widget

--- to_do_flutter/lib/main.dart
+++ to_do_flutter/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
@@ -14,52 +16,26 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-          backgroundColor: Colors.deepPurple[200],
-          appBar: AppBar(
-              title: Center(child: Text("Test App bar")),
-              backgroundColor: Colors.deepPurple[400],
-              leading: Icon(Icons.menu),
-              actions: [
-                IconButton(onPressed: () {}, icon: Icon(Icons.search)),
-                IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
-              ]),
-          body: ListView.builder(
-              itemCount: names.length,
-              itemBuilder: (context, index) => ListTile(
-                    title: Text(names[index]),
-                  ))),
-      // ListView(
-      //   scrollDirection: Axis.vertical, // Can be horizontal or vertical
-      //   children: [
-
-      //     Expanded(
-      //         flex: 1, child: Container(height: 350, color: Colors.red)),
-      //     Expanded(
-      //         flex: 2, child: Container(height: 350, color: Colors.blue)),
-      //     Expanded(
-      //       child: Container(
-      //         height: 500,
-      //         decoration: BoxDecoration(
-      //           color: Colors.deepPurple,
-      //           borderRadius: BorderRadius.circular(20.0),
-      //         ),
-      //         padding: EdgeInsets.all(25.0),
-      //         // padding: EdgeInsets.only(left: 10.0),
-      //         // padding: EdgeInsets.symmetric(horizontal: 10.0,vertical:20.0),
-      //         child: Text(
-      //           "TEST text widget",
-      //           style: TextStyle(
-      //               color: Colors.white,
-      //               fontSize: 20,
-      //               fontWeight: FontWeight.bold),
-      //         ),
-      //       ),
-      //     )
-      //     // color: Colors.deepPurple,
-      //   ],
-      // )),
-    );
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+            backgroundColor: Colors.deepPurple[200],
+            appBar: AppBar(
+                title: Center(child: Text("Test App bar")),
+                backgroundColor: Colors.deepPurple[400],
+                leading: Icon(Icons.menu),
+                actions: [
+                  IconButton(onPressed: () {}, icon: Icon(Icons.search)),
+                  IconButton(onPressed: () {}, icon: Icon(Icons.logout)),
+                ]),
+            body: GridView.builder(
+                itemCount: 64,
+                gridDelegate:
+                    // Grid delegate controls how many items are displayed in a row
+                    SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 4),
+                itemBuilder: (context, index) => Container(
+                      color: Colors.deepPurple,
+                      margin: EdgeInsets.all(2),
+                    ))));
   }
 }
